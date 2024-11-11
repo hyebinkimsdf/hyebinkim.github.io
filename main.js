34f0ef1601848d15a31bacde6edafb5ba4888fd1
김혜빈 <hyebinkimdesign@gmail.com>
2024-11-11 14:34:09 +0900

    feat: GNB 구현

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ menuItems.forEach(item => {
         this.classList.add('active');
     });
 });
+
+//좋아요 버튼 기능
+function favBtn() {
+    const favBtn = document.querySelector(".favourite_btn img");
+    favBtn.addEventListener("click", function () {
+        // 현재 src에 따라 이미지 변경
+        if (favBtn.src.includes("favourite_bnt.svg")) {
+            favBtn.src = "./icon/favourite_bnt_0.svg";
+        } else {
+            favBtn.src = "./icon/favourite_bnt.svg";
+        }
+    });
+}
+
+favBtn();
+
+
