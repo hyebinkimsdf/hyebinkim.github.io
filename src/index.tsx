369b0ae509a8890d88f3f946370e453f81f36440
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-01 04:12:34 +0900

    fix: 라우터 설정 수정

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,6 @@ import { App } from "./router/App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Header } from "../components/Header";
+import { Outlet } from "react-router-dom";
 
 export function App() {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 }
