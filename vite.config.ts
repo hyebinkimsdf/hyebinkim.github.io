6630cc244038daba988cc7c72d3ff5cb2f4250e2
김혜빈 <hyebinkimdesign@gmail.com>
2025-02-10 22:06:52 +0900

    fix: vite 오류 수정

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/",
+  base: "",
   plugins: [react(), svgr()],
   optimizeDeps: {
     include: [
